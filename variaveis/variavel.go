2a869d7c458f856ccb9e37607f15935a2d93c05b
Paula Salino Ribeiro <paula.ribeiro@easylive.com.br>
2023-09-21 14:24:20 -0300

    add variaveis

---
+++ variaveis/variavel.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variavel1 string = "texto da variavel 1"
+	variavel2 := "texto da variavel 2"
+
+	fmt.Println(variavel1)
+	fmt.Println(variavel2)
+}
